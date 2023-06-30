65fb63e46453d997a36f0db0c328f9f86e111bb9
marcelo <mhbcoelho99@gmail.com>
2023-06-30 20:17:50 -0300

    remastering

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
-import Image from 'next/image'
 import Header from '../components/header'
-import SidebarLeft from '../components/home/sidebarLeft/sidebarLeft'
+
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <div className='p-2 mt-14'>
-        <SidebarLeft/>
-      </div>
     </>
   )
 }
