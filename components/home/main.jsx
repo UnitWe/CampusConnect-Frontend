c0a8b91e629f01ac7bc30db3a37102cf44f4766b
marcelo <mhbcoelho99@gmail.com>
2023-07-04 17:17:55 -0300

    editing url using regex

--- components/home/main.jsx
+++ components/home/main.jsx
@@ -7,23 +7,34 @@ export default function main() {
     const { error, loading, request } = useFetch();
     const url = "http://localhost:8000/"
 
-   
+
 
     React.useEffect(() => {
         const fetchData = async () => {
-          const options = {
-            method: 'GET'
-          };
+            const options = {
+                method: 'GET'
+            };
 
-          const {response, json} = await request(url, options);
-   
-          if (response.ok) {
-            setPosts(json.data)
-          }
+            const { response, json } = await request(url, options);
+
+            if (response.ok) {
+                setPosts(json.data)
+            }
         };
-    
+
         fetchData();
     }, [request, url]);
+
+    const createSlug =(text) => {
+        const slug = text
+          .toLowerCase()
+          .replace(/[^\w\s-]/g, '') // Remove caracteres especiais
+          .replace(/\s+/g, '-') // Substitui espaços por hífens
+          .replace(/--+/g, '-') // Remove hífens duplicados
+          .trim(); // Remove espaços em branco no início e no fim
+      
+        return slug;
+    }
 
 
 
@@ -31,23 +42,32 @@ export default function main() {
         <main className="mt-20 px-4">
             <div className="max-w-4xl m-auto mb-12">
 
-            {loading && <p>Carregando...</p>}
+                {loading && <p>Carregando...</p>}
 
                 {Array.isArray(posts) ? (
-                    posts.map((post, index) => (
-                        <div key={post.id} className="mb-3" >
-                            <a href={`/${post.user}/${post.title}?post_id=${post.id}`} className="hover:underline" >
-                                {index + 1}. {post.title}
-                            </a>
-                            <div className="flex items-center gap-2 ml-4 mt-0.5">
-                                <span className="text-xs text-zinc-400">4 tabcoins</span>
-                                <span className="text-xs text-zinc-400">12 comentários</span>
-                                <a href={`/${post.user}`} className="text-xs text-zinc-400 hover:underline">{post.user}</a>
-                                <span className="text-xs text-zinc-400">19 horas</span>
+                    posts.map((post, index) => {
+                        const slug = createSlug(post.title);
+                        const postUrl = `/${post.user}/${slug}?post_id=${post.id}`;
+
+                        return (
+                            <div key={post.id} className="mb-3">
+                                <a href={postUrl} className="hover:underline">
+                                    {index + 1}. {post.title}
+                                </a>
+                                <div className="flex items-center gap-2 ml-4 mt-0.5">
+                                    <span className="text-xs text-zinc-400">4 tabcoins</span>
+                                    <span className="text-xs text-zinc-400">12 comentários</span>
+                                    <a href={`/${post.user}`} className="text-xs text-zinc-400 hover:underline">
+                                        {post.user}
+                                    </a>
+                                    <span className="text-xs text-zinc-400">19 horas</span>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                ) : <p>Nenhum post encontrado.</p>}
+                        );
+                    })
+                ) : (
+                    <p>Nenhum post encontrado.</p>
+                )}
 
             </div>
         </main>
