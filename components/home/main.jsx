669ab31da4d39016f90e4db0f4a3e4f2f4141a44
marcelo <mhbcoelho99@gmail.com>
2023-07-04 09:45:00 -0300

    using useFetch

--- components/home/main.jsx
+++ components/home/main.jsx
@@ -1,38 +1,48 @@
 'use client'
-import Link from "next/link"
 import React from "react"
+import useFetch from "../hooks/useFetch"
 export default function main() {
 
-
     const [posts, setPosts] = React.useState([]);
-    React.useEffect(() => {
-        fetchPosts();
-    }, []);
+    const { error, loading, request } = useFetch();
+    const url = "http://localhost:8000/"
 
-    const fetchPosts = async () => {
-        try {
-            const response = await fetch('http://localhost:8000/');
-            const responseData = await response.json();
-            const data = responseData.data; // Acessa a propriedade "data" da resposta
-            setPosts((prevPosts) => [...prevPosts, ...data]);
-        } catch (error) {
-            console.error('Erro ao buscar os posts:', error);
-        }
-    };
+   
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+          const options = {
+            method: 'GET'
+          };
+
+          const {response, json} = await request(url, options);
+   
+          if (response.ok) {
+            setPosts(json.data)
+          }
+        };
+    
+        fetchData();
+    }, [request, url]);
+
+
 
     return (
         <main className="mt-20 px-4">
             <div className="max-w-4xl m-auto mb-12">
+
+            {loading && <p>Carregando...</p>}
+
                 {Array.isArray(posts) ? (
                     posts.map((post, index) => (
                         <div key={post.id} className="mb-3" >
-                            <Link href={`/${post.user}/${post.id}?post_id=${post.id}`} prefetch={false} className="hover:underline" >
+                            <a href={`/${post.user}/${post.title}?post_id=${post.id}`} className="hover:underline" >
                                 {index + 1}. {post.title}
-                            </Link>
+                            </a>
                             <div className="flex items-center gap-2 ml-4 mt-0.5">
                                 <span className="text-xs text-zinc-400">4 tabcoins</span>
                                 <span className="text-xs text-zinc-400">12 comentários</span>
-                                <Link href={`/${post.user}`} className="text-xs text-zinc-400 hover:underline">{post.user}</Link>
+                                <a href={`/${post.user}`} className="text-xs text-zinc-400 hover:underline">{post.user}</a>
                                 <span className="text-xs text-zinc-400">19 horas</span>
                             </div>
                         </div>
