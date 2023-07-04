5421c5c15f300c7f760da433771fb375b83b549e
marcelo <mhbcoelho99@gmail.com>
2023-07-04 07:51:48 -0300

    create post done

---
+++ app/create_post/page.js
@@ -0,0 +1,11 @@
+import Header from "../../components/header"
+import Newpost from "../../components/newpost/newpost"
+
+export default function newpost() {
+    return (
+        <>
+            <Header/>
+            <Newpost/>
+        </>
+    )
+}
