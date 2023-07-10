8df9ce8e45bdcd372d43eed0466254eb2ad4303a
marcelo <mhbcoelho99@gmail.com>
2023-07-10 16:45:48 -0300

    adding showTime in unixtimestamp

---
+++ functions/timeDifference.jsx
@@ -0,0 +1,37 @@
+export default function timeDifference(previous) {
+    const unixPreviosTime = new Date(previous).getTime()
+    const current = Date.now()
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+    
+    var elapsed = current - unixPreviosTime;
+    
+    if (elapsed < msPerMinute) {
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+    
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+    
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+         return + Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+    
+    else if (elapsed < msPerYear) {
+         return + Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+    
+    else {
+    			console.log('oi')
+         return + Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+
+}
