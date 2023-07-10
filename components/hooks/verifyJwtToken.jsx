523106f10eb5cb3eadbf026d34c56182bf7849b4
marcelo <mhbcoelho99@gmail.com>
2023-07-10 11:54:11 -0300

    verifyJwtToken hook

---
+++ components/hooks/verifyJwtToken.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import * as jose from 'jose';
+
+export default function verifyJwtToken() {
+    const [username, setUsername] = React.useState('');
+    const [error, setError] = React.useState(null);
+    
+
+    const verify = React.useCallback(async (token) => {
+        let decoded = null;
+        try {
+            const EncodedSecretKey = new TextEncoder().encode('asdsddasybudsa');
+            decoded = await jose.jwtVerify(token, EncodedSecretKey, { algorithms: ['HS256'] });
+        } catch (e) {
+            setError(e);
+        } 
+            
+        return { decoded, error }
+        
+    }, [])
+   
+
+    return {
+        username,
+        error,
+        verify,
+      };
+};
+
+
+
